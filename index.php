fdc2bb68a00ce26fea9c91bc61e26a4a770a70a0
Carlo-Adile <adile.carlo.a@gmailcom>
2024-04-17 16:08:14 +0200

    init, header, install boostrap and vue via cdn

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <title>Discografia JSON</title>
+</head>
+<body>
+  
+<header id="my_header">
+<nav class="navbar">
+  <div class="container-fluid ps-4">
+    <a class="navbar-brand" href="#">
+      <img src="assets/img/spotify_logo.png" alt="Bootstrap" width="60" height="48">
+    </a>
+  </div>
+</nav>
+</header>
+
+<main>
+<div class="container-xl">
+  <div class="row">
+    <div class="col">
+      -<div class="card">
+        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
+        <div class="card-body">
+          <h4 class="card-title">Title</h4>
+          <p class="card-text">Text</p>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+</main>
+
+<footer>
+
+</footer>
+
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
